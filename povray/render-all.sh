a78ff862874302c795be6682770cf4aa04a6d8b2
Eric Willisson <ericwillisson@gmail.com>
2015-07-04 17:48:45 -0400

    Maybe rewriting in Go

--- povray/render-all.sh
+++ povray/render-all.sh
@@ -3,6 +3,6 @@
 # A Makefile could probably do this but since the other one is a script,
 # I'm making this one one as well.
 
-#render.sh Room_N Room_N_red Red_Room=1
-#render.sh Room_N Room_N_blue Blue_Room=1
+render.sh Room_N Room_N_red Red_Room=1
+render.sh Room_N Room_N_blue Blue_Room=1
 
